73f305e0cc9a22d943be2869e5322abe2312ff5c
yoon <kimyam1008@gmail.com>
2024-11-26 15:21:51 +0900

    new file:   src/App.vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul></ul>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            storedResources: [
+                {
+                    id: 'official-guide',
+                    title: 'Official Guide',
+                    description: 'The official Vue.js documentation.',
+                    link: 'https://vuejs.org'
+                },
+                {
+                    id: 'google',
+                    title: 'Google',
+                    description: 'Learn to goolge',
+                    link: 'https://goolge.org'
+                },
+            ]
+        };
+    }
+}
+</script>
